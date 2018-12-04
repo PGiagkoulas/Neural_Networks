bb8f403129c02be3c63841fe15fbaf53603f9e67
PGiagkoulas <pgiagkoulas@gmail.com>
2018-12-04 13:24:19 +0100

    Added termination criterion on E for training

--- perceptron.m
+++ perceptron.m
@@ -1,7 +1,9 @@
+clear 
+clc
 % number of datasets
-P = 40;
+P = 100;
 % feature dimensions
-N = 20;
+N = 80;
 % epochs
 n = 50;
 % max epochs
@@ -11,17 +13,21 @@ w = zeros(1, N);
 % generate features PxN matrix and labels in 1xP vector
 features = rnd_feature_gen(P, N);
 labels = rnd_label_gen(P);
-% for every training epoch
-for epoch = 1:nmax
+epoch = 0;
+all_e = zeros(1, P);
+% until you have trained for nmax epochs or all E are positive
+while epoch<=nmax && (length(all_e(all_e>0)) < P)
+    pos_e = 0;
+    epoch = epoch +1;
     % for every dataset
     for t = 1:P
        % cacl E
-       E = transpose(w)*(features(t,:))*labels(t);
+       all_e(t) = w*(transpose(features(t,:)))*labels(t);
        % decide based on E, how to update the weights
-       if E <= 0
+       if all_e(t) <= 0
            w = w + (1/N).*features(t,:)*labels(t);
        end
-    end    
+    end
 end
 fprintf("After %d epochs the weights are: \n", epoch);
 w
